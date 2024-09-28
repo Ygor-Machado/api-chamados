7462083fd46ccb03fcb880123a40d5493ed3e9c2
Ygor-Machado <ygaounderground011@gmail.com>
2024-09-28 02:14:51 -0300

    feat: cria UserController, AuthController e middleware

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = User::all();
+
+        return response()->json($users);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, User $users)
+    {
+        $users->create($request->all());
+
+        return response()->json($users);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        return response()->json($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        $user->update($request->all());
+
+        return response()->json($user);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return response()->json($user);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,21 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DepartamentController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
 
-Route::apiResource('departaments', DepartamentController::class);
+
+Route::middleware(['auth:sanctum', AdminMiddleware::class ])->group(function () {
+    Route::apiResource('departaments', DepartamentController::class);
+    Route::apiResource('users', UserController::class);
+});
+
+Route::post('/login', [AuthController::class, 'login']);
+
