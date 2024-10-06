8c86540b51487c0ebe3ccf52773dffde88608e61
Ygor Machado <ygor@DESKTOP-MGPDND4.>
2024-10-06 18:11:49 -0300

    feat: ao retornar usuários retorna também os tickets criados por ele

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::all();
+        $users = User::with('tickets')->get();
 
         return response()->json($users, Response::HTTP_OK);
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
 }
